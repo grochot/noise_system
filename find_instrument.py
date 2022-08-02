2ee4810b056b0f49ae40464a1eb661559b00e1f4
grochot <47087472+grochot@users.noreply.github.com>
2022-08-02 21:45:43 +0200

    sd

--- find_instrument.py
+++ find_instrument.py
@@ -5,7 +5,7 @@ class FindInstrument():
     def __init__(self): 
         rm = pyvisa.ResourceManager() 
         print(rm.list_resources()) 
-        inst = rm.open_resource('/dev/ttyS0::INSTR')
+        inst = rm.open_resource('ASRL/dev/ttyS0::INSTR')
         print(inst.query("*IDN?"))
 
 
