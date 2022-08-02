f16131cbddaa0e815c4fec412c2a584e5a51390d
grochot <47087472+grochot@users.noreply.github.com>
2022-08-02 20:38:37 +0200

    create_instr_class

---
+++ find_instrument.py
@@ -0,0 +1,15 @@
+import pyvisa 
+
+
+class FindInstrument(): 
+    def __init__(self): 
+        rm = pyvisa.ResourceManager() 
+        rm.list_resources() 
+        inst = rm.open_resource('GPIB0::12::INSTR')
+        print(inst.query("*IDN?"))
+
+
+
+inst = FindInstrument() 
+
+
