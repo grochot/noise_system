18694363d9cd44755d01868cb2ffba3295cb0453
grochot <47087472+grochot@users.noreply.github.com>
2022-08-03 21:22:44 +0200

    edit

--- find_instrument.py
+++ find_instrument.py
@@ -3,13 +3,21 @@ import pyvisa
 
 class FindInstrument(): 
     def __init__(self): 
-        rm = pyvisa.ResourceManager() 
-        print(rm.list_resources()) 
-        inst = rm.open_resource('ASRL/dev/ttyS0::INSTR')
-        print(inst.query("*IDN?"))
+        self.rm = pyvisa.ResourceManager() 
+        self.hardware = {}
+    def find_instrument(self):
+        for i in self.rm.list_resources(): 
+            print(i) 
+            inst = self.rm.open_resource(i)  
+            self.hardware[inst.query('*IDN?')] = i
+            inst.close()
+        #inst = rm.open_resource('ASRL/dev/ttyS0::INSTR')
+        #2print(inst.query("*IDN?"))
+        return self.hardware
 
 
 
 inst = FindInstrument() 
+
 
 
